633cd43372b716b311fcc034ecb26509bad78ceb
RAM <v07787805@gmail.com>
2025-12-29 08:58:58 +0530

    Fix Vercel serverless crash

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import connectDB from './configs/db.js';
 import {inngest,functions} from './inngest/index.js'
 import {serve} from 'inngest/express'
 const app=express();
-await connectDB();
+connectDB();
 
 app.use(express.json());
 app.use(cors());
@@ -14,4 +14,3 @@ app.get('/',(req,res)=> res.send('server is running'))
 app.use('/api/inngest',serve({client:inngest,functions}))
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
