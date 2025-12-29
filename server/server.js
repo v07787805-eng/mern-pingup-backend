5d15d2113aba2c39ca1cb46cec954c733999f429
RAM <v07787805@gmail.com>
2025-12-29 12:29:10 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -15,4 +15,9 @@ app.get('/', (req, res) => res.send('Server is running'));
 app.use('/api/inngest', serve({ client: inngest, functions }));
 
 // Export the app for Vercel serverless
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 export default app;
