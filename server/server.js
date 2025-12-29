1b5658a8c05186808f09e8437915060154bf9b3e
RAM <v07787805@gmail.com>
2025-12-29 08:52:40 +0530

    Fix inngest server import

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import {inngest,functions} from './inngest/index.js'
+import {serve} from 'inngest/express'
 const app=express();
 await connectDB();
 
